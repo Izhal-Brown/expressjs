cd5ba6aeb5215974de7d5f4bb75a52d982b51cbb
Izhal-Brown <informatika749@gmail.com>
2022-08-31 15:59:57 +0800

    membuat web Expressjs

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+
+
+// membuat layout :
+app.set('view engine', 'ejs')
+
+// mengambil berbagai asset di dalam direktori public
+app.use(express.static('public'))
+
+
+// membuat route untuk masing" halaman:
+app.get('/', function (req, res) {
+  res.render('pages/biografi')
+})
+
+app.get('/skill', function (req, res) {
+  res.render('pages/skill')
+})
+
+app.get('/contactme', function (req, res) {
+  res.render('pages/contactme')
+})
+
+app.get('/myclosefriend', function (req, res) {
+  res.render('pages/myclosefriend')
+})
+
+app.listen(3000,function(){
+  console.log('server is okay http://localhost:3000')
+})
